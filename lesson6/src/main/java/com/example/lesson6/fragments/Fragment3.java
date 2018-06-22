a03726f49e7401454429c891540be16797038678
Anton <30378794+fr0zen87@users.noreply.github.com>
2018-06-22 17:50:10 +0300

    added lessons 5 and 6

--- lesson6/src/main/java/com/example/lesson6/fragments/Fragment3.java
+++ lesson6/src/main/java/com/example/lesson6/fragments/Fragment3.java
@@ -1,5 +1,9 @@
 package com.example.lesson6.fragments;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +11,77 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.lesson6.R;
+import com.example.lesson6.services.Service1;
 
 public class Fragment3 extends Fragment {
+
+    public static final String BROADCAST_ACTION_3 = "com.example.lesson6.fragments.BROADCAST_ACTION_3";
+    public static final String TEXT_RETURN = "text result";
+    public static final String TEXT_BUTTON = "text button";
+
+    private Button button1;
+    private Button button2;
+    private Button button3;
+
+    private BroadcastReceiver3 receiver3;
+    private IntentFilter intentFilter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3, container, false);
+        View view = inflater.inflate(R.layout.fragment3, container, false);
+
+        button1 = view.findViewById(R.id.fr3_button1);
+        button2 = view.findViewById(R.id.fr3_button2);
+        button3 = view.findViewById(R.id.fr3_button3);
+
+        receiver3 = new BroadcastReceiver3();
+        intentFilter = new IntentFilter(BROADCAST_ACTION_3);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().registerReceiver(receiver3, intentFilter);
+
+        startService3();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(receiver3);
+    }
+
+    private void startService3() {
+        Intent intent = Service1.newIntent(getContext());
+        getActivity().startService(intent);
+    }
+
+    private class BroadcastReceiver3 extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String text = intent.getStringExtra(TEXT_RETURN);
+            int colorButton = intent.getIntExtra(TEXT_BUTTON, 1);
+            switch (colorButton) {
+                case 1: {
+                    button1.setText(text);
+                    break;
+                }
+                case 2: {
+                    button2.setText(text);
+                    break;
+                }
+                case 3: {
+                    button3.setText(text);
+                    break;
+                }
+            }
+        }
     }
 }
--- lesson6/src/main/java/com/example/lesson6/services/Service3.java
+++ lesson6/src/main/java/com/example/lesson6/services/Service3.java
@@ -1,8 +1,14 @@
 package com.example.lesson6.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+
+import com.example.lesson6.fragments.Fragment3;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Service3 extends IntentService {
 
@@ -12,6 +18,27 @@ public class Service3 extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            Random random = new Random();
+            String text;
+            int textButton;
+            Intent broadcastIntent = new Intent(Fragment3.BROADCAST_ACTION_3);
+            for (int i = 1; i <= 100; i++) {
+                text = "Iteration: " + i;
+                textButton = random.nextInt(3) + 1;
+                intent.putExtra(Fragment3.TEXT_BUTTON, textButton);
+                intent.putExtra(Fragment3.TEXT_RETURN, text);
+                sendBroadcast(broadcastIntent);
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
+    public static Intent newIntent(Context context) {
+        return new Intent(context, Service3.class);
     }
 }
